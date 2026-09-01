9a9284ddc2174fe304e5077c474f1533d136c5b4
ericchang9 <changeri39@gmail.com>
2026-08-31 21:01:22 -0400

    Added Signals to serializer

--- src/Video_Driver/serializer.sv
+++ src/Video_Driver/serializer.sv
@@ -1,7 +1,13 @@
 // Basic module syntax with port list
-module <name> ([port_list]);
-	// Variable declarations (wires, regs, integers, etc.)
-	// Dataflow statements (assign, always, initial blocks)
-	// Function and task definitions
-	// Sub-module instantiations
+module serializer (
+    input logic clk_pixel, //Pixel Clock
+    input logic clk_serial, //Serial Clock whihc 5x the speed
+    input logic n_rst,
+    input logic [9:0] data_in,
+    output logic [9:0] serial_p,
+    output logic [9:0] serial_n
+);
+
+
+
 endmodule
